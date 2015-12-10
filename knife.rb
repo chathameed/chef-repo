0d914e05af8389838daa2f416394d3f64770afc9
muthu-s <laksh.actech@gmail.com>
2015-12-10 10:56:49 -0600

    Updated knife.rb

--- knife.rb
+++ knife.rb
@@ -3,7 +3,7 @@ log_location             STDOUT
 node_name                'muthulakshmanan'
 client_key               'C:/Users/ms66518/chef-repo/muthulakshmanan.pem'
 validation_client_name   'laksh-validator'
-validation_key           'C:/Users/ms66518/.chef/chef-validator.pem'
+validation_key           'C:/Users/ms66518/.chef/laksh-validator.pem'
 chef_server_url          'https://api.chef.io/organizations/laksh'
 syntax_check_cache_path  'C:/Users/ms66518/chef-repo/syntax_check_cache'
 cookbook_path [ 'C:/Users/ms66518/chef-repo//cookbooks' ]
