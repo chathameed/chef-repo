def5aecacbc0662d6ec5338f494037b03a43a536
muthu-s <laksh.actech@gmail.com>
2015-12-10 11:00:14 -0600

    updated knife.rb

--- knife.rb
+++ knife.rb
@@ -1,7 +1,7 @@
 log_level                :info
 log_location             STDOUT
-node_name                'muthulakshmanan'
-client_key               'C:/Users/ms66518/chef-repo/muthulakshmanan.pem'
+node_name                'Muthu-Laptop'
+client_key               'C:/Users/ms66518/.chef/muthulakshmanan.pem'
 validation_client_name   'laksh-validator'
 validation_key           'C:/Users/ms66518/.chef/laksh-validator.pem'
 chef_server_url          'https://api.chef.io/organizations/laksh'
