d3e2b2f806aa9e46378025deb060d9ff1896ca3c
muthu-s <laksh.actech@gmail.com>
2015-12-16 10:16:27 -0600

    Added IHS Config cookbook and Self signed kdb files

--- cookbooks/ihs_config/recipes/default.rb
+++ cookbooks/ihs_config/recipes/default.rb
@@ -6,3 +6,55 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+
+ihs_docroot_dir = node[:ihs][:docroot_dir]
+ihs_log_dir = node[:ihs][:log_dir]
+ihs_key_file = node[:ihs][:key_file]
+ihs_stash_file = node[:ihs][:stash_file]
+
+[ihs_docroot_dir, ihs_log_dir].each do |dirname|
+  directory dirnmae do
+    mode '755'
+    recursive true
+    owner node[:ihs][:user]
+    group node[:ihs][:group]
+    action :create
+  end
+end
+
+cookbook_file ihs_key_file do
+  source "#{node[:ihs][:cert_CN]}.kdb"
+  owner node[:ihs][:user]
+  group node[:ihs][:group]
+  action :create
+end
+
+cookbook_file ihs_stash_file do
+  source "#{node[:ihs][:cert_CN]}.sth"
+  owner node[:ihs][:user]
+  group node[:ihs][:group]
+  action :create
+end
+
+template "#{node[:ihs][:conf_dir]}/httpd.conf" do
+  source 'httpd.conf.erb'
+  action :create
+  owner node[:ihs][:user]
+  group node[:ihs][:group]
+  mode '755'
+  notifies :restart, 'service[ibm-http]', :delayed
+end
+
+template "#{node[:ihs][:conf_dir]}/ssl.conf" do
+  source 'ssl.conf.erb'
+  action :create
+  owner node[:ihs][:user]
+  group node[:ihs][:group]
+  mode '755'
+  notifies :restart, 'service[ibm-http]', :delayed
+end
+
+service 'ibm-http' do
+  supports :restart => true, :reload => true
+  action :nothing
+end
