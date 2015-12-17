bc009890ec751a475ffb52f499cd4533fb2978d1
muthu-s <laksh.actech@gmail.com>
2015-12-17 16:46:44 -0600

    Tested 64 bit OS successfully

--- cookbooks/ihs_config/recipes/default.rb
+++ cookbooks/ihs_config/recipes/default.rb
@@ -6,13 +6,15 @@
 #
 # All rights reserved - Do Not Redistribute
 #
-
+ihs_shared_path = node[:ihs][:paths][:shared]
+ihs_install_path = node[:ihs][:paths][:install]
+ihs_plugin_path = node[:ihs][:paths][:plugins]
 ihs_docroot_dir = node[:ihs][:docroot_dir]
 ihs_log_dir = node[:ihs][:log_dir]
 ihs_key_file = node[:ihs][:key_file]
 ihs_stash_file = node[:ihs][:stash_file]
 
-[ihs_docroot_dir, ihs_log_dir].each do |dirname|
+[ihs_shared_path, ihs_install_path, ihs_plugin_path, ihs_docroot_dir, ihs_log_dir].each do |dirname|
   directory dirname do
     mode '755'
     recursive true
@@ -52,6 +54,13 @@ template "#{node[:ihs][:conf_dir]}/ssl.conf" do
   group node[:ihs][:group]
   mode '755'
   notifies :restart, 'service[ibm-http]', :delayed
+end
+
+template "#{node[:ihs][:docroot_dir]}/index.html" do
+  source 'index.html.erb'
+  action :create
+  owner node[:ihs][:user]
+  group node[:ihs][:group]
 end
 
 service 'ibm-http' do
