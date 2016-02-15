4e3bae1744378ac6d898c1ef41b4681c7453aad3
muthu-s <laksh.actech@gmail.com>
2016-02-15 16:51:09 -0600

    Added Siteminder recipes

---
+++ cookbooks/siteminder/recipes/register_web_agent.rb
@@ -0,0 +1,22 @@
+policy_server_1 = node['siteminder']['policy_server_1']
+sm_user = node['siteminder']['sm_user']
+sm_password = node['siteminder']['sm_password']
+hostname = node['siteminder']['hostname']
+hco = node['siteminder']['hco']
+host_config_file = node['siteminder']['host_config_file']
+
+template "#{node['siteminder']['ihs_conf_root']}/WebAgent.conf" do
+  source 'WebAgent.conf.erb'
+  action :create
+  owner node['siteminder']['ihs_user']
+  group node['siteminder']['ihs_group']
+  mode '755'
+end
+
+bash 'register_siteminder' do
+  cwd "#{node['siteminder']['webagent_lib']}"
+  code <<-EOH
+  source /apps/siteminder/webagent/nete_wa_env.sh
+  ./smreghost -i #{policy_server_1} -u #{sm_user} -p #{sm_password} -hn #{hostname} -hc #{hco} -f #{host_config_file}
+  EOH
+end
